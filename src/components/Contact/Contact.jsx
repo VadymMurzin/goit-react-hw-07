8b2662fbda7c55d667b69981773d45013e9acfa4
VadymMurzin <prometey736@gmail.com>
2024-03-08 10:07:26 +0100

    Update Contact and operators

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,33 @@
+// import { useDispatch } from 'react-redux';
+// import css from './contact.module.css';
+// import { deleteContact } from '../../redux/operators';
+
+// export default function Contact({ contacts }) {
+//   const dispatch = useDispatch();
+//   const handleDelete = () => dispatch(deleteContact(contacts.id));
+//   console.log(contacts);
+//   return (
+//     <div>
+//       <h2 className={css.text}>
+//         {contacts.name}
+//       </h2>
+//       <p className={css.text}>
+//         {contacts.phone}
+//       </p>
+//       <button onClick={handleDelete} className={css.button} type="button">
+//         Delete
+//       </button>
+//     </div>
+//   );
+// }
+// Contact.jsx
 import { useDispatch } from 'react-redux';
 import css from './contact.module.css';
-import { deleteContact } from '../../redux/operators';
+import { deleteContactAndUpdateList } from '../../redux/operators';
 
 export default function Contact({ contacts }) {
   const dispatch = useDispatch();
-  const handleDelete = () => dispatch(deleteContact(contacts.id));
+  const handleDelete = () => dispatch(deleteContactAndUpdateList(contacts.id)); // Змінено на нову дію
   console.log(contacts);
   return (
     <div>
